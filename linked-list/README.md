247497d95214b7ae8ac45c95d8db40503f84033f
abdelhai <abdelhaijamal95@gmail.com>
2023-12-11 17:10:06 +0200

    Shift and Pop methods added

--- linked-list/README.md
+++ linked-list/README.md
@@ -5,10 +5,18 @@
 ## SinglyLinkedList 
 - This is the class that will contain all of the functions and is in charge of making linked lists.
 
-## Push() 
-- Inserts element to the end of the list.
+## push() 
+- Inserts a node to the end of the list.
 - Check if the list is empty. 
 - Make the *head* and *tail* points to the newely created node.
 - If there are nodes update the tail *next*property to point to the new node and then make set the tail to be equal to the new node.
 - Lines 23/24 will change the `head.next` property because they refer to the same object and by changing it through the tail head will be changed too.
 
+## pop() 
+- Removes a node from the end of the list.
+- We traverse the list starting from the *head* to Find the last element to the *tail* I called it `preLastElement`.
+- Set it's `next` property to be *null*.
+- Set this element to be the tail.
+- Decrement the length by one.
+- Return the old tail I stored it in `popedElement`.
+- `temp` used to traverse the list.
---
+++ linked-list/solution.js
@@ -0,0 +1,59 @@
+class Node{
+    constructor(val) {
+        this.val = val;
+        this.next = null;
+    }
+}
+
+
+class SinglyLinkedList{
+    constructor(){
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
+    }
+
+    push(val) {
+        let newNode = new Node(val);
+        if(!this.head) {
+            this.head = newNode;
+            this.tail = newNode;
+        } 
+        else {
+            this.tail.next = newNode;
+            this.tail = newNode;
+        }
+
+        ;
+        this.length++;
+        return this;
+    }
+
+
+    pop() {
+        if(! this.length) return -1;
+        let temp = this.head;
+        let preLastElement, popedElement;
+        while(temp) {
+            if(temp.next === this.tail) {
+                preLastElement = temp;
+                popedElement = this.tail;
+                preLastElement.next = null;
+                this.tail = preLastElement;
+            }
+            temp = temp.next;
+        }
+
+        this.length--;
+        return popedElement;
+    }
+}
+
+
+let list = new SinglyLinkedList();
+
+// Tests
+list.push(5);
+list.push(6);
+list.push(7);
+list.pop();
